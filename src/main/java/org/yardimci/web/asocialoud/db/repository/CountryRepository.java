b9c7ddbaa3edf952fda789706cd25b4e0747ee15
asyard <asyard@gmail.com>
2018-09-09 01:15:21 +0300

    country data and rest controller

---
+++ src/main/java/org/yardimci/web/asocialoud/db/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package org.yardimci.web.asocialoud.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.yardimci.web.asocialoud.db.model.Country;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+}
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+MERGE INTO tbl_country (id, name) VALUES (1, 'Turkey');
+MERGE INTO tbl_country (id, name) VALUES (2, 'Greece');
+MERGE INTO tbl_country (id, name) VALUES (3, 'Spain');
+MERGE INTO tbl_country (id, name) VALUES (4, 'Italy');
