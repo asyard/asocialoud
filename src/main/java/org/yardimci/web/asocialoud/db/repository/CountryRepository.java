10fe0109474a3d44213cf39c1da69b5f4a5fc14a
asyard <asyard@gmail.com>
2019-05-05 15:55:09 +0300

    no need for country service

--- src/main/java/org/yardimci/web/asocialoud/db/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.yardimci.web.asocialoud.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.yardimci.web.asocialoud.db.model.Country;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-
-
-}
